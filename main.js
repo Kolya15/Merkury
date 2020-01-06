98c2446453ae713bd02ad0aa809e7fa65886a5f0
Kolya15 <kolya.soroka15@gmail.com>
2020-01-06 18:33:47 +0200

    Правки адаптивности

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Выпадающий список
 let btnDropMenu = document.querySelector(".btn__drop-menu");
 let wrapperDropMenu = document.querySelector(".wrapper__drop-menu");
 let wrapperDropMenuActive = document.querySelector(
@@ -7,3 +8,19 @@ let wrapperDropMenuActive = document.querySelector(
 btnDropMenu.addEventListener("click", function() {
   wrapperDropMenu.classList.toggle("wrapper__drop-menu_active");
 });
+
+//Скрол навигации
+const anchors = document.querySelectorAll("a.scroll-to");
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    wrapperDropMenu.classList.remove("wrapper__drop-menu_active");
+    const blockID = anchor.getAttribute("href");
+    wrapperDropMenu.classList.remove("wrapper__drop-menu_active");
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+}
